6c8f45dfb5ced26c5feb28e7e9f09c16839d9146
Kunshi <you@example.com>
2019-06-22 23:59:41 +0900

    トップ画面修正2

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -44,7 +44,9 @@
                 <div class="text-center" style="font-size:0.8em;">{{ $item->name }}</div>
                 <!-- <p class="card-text" style="font-size:0.5em;">定価：{{ $item->list_price }}</p> -->
                 <div class="text-center text-danger" style="font-size:0.5em;">価格(税込)：<br><?php echo number_format($item->sale_price); ?>円</div>
-                <a href="./show?id={{ $item->id }}" class="text-center" style="font-size:0.7em;">詳細を見る</a>
+                <div class="text-center">
+                  <a href="./show?id={{ $item->id }}" class="" style="font-size:0.7em;">詳細を見る</a>
+                </div>
                 <div class="text-center" style="font-size:0.3em;">品番：{{ $item->item_code }}</div>
               </div>
             </div>
